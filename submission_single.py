a8fafaca5c04ab77e0b2cda1f4198719bac236a7
Stephen Haskins <haskinss@tcd.ie>
2025-05-10 22:03:16 +0200

    03 dropout changes 98 reward

--- submission_single.py
+++ submission_single.py
@@ -335,7 +335,7 @@ def algo_config(id_env, env, policies, policies_to_train):
                             "input_dim": env.observation_space(x).shape[0],
                             "fcnet_use_batch_norm": True,  # Enable batch normalization
                             "fcnet_batch_norm_momentum": 0.99,  # Momentum for batch norm
-                            "fcnet_dropout": 0.2,  # Dropout rate of 20%
+                            "fcnet_dropout": 0.3,  # Dropout rate of 20%
                             "fcnet_dropout_training": True  # Enable dropout during training
                         }
                     )
